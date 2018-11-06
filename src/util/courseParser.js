a6d941db85287a3fb7d226f1814c84132487f82e
EvilKanoa <kanoa@kanoa.ca>
2018-11-06 10:15:52 -0500

    Removed client-side course parser

--- src/util/courseParser.js
+++
@@ -1,7 +0,0 @@
-export const domParser = new DOMParser();
-
-export const parseCourses = (html) => {
-    const doc = domParser.parseFromString(html, 'text/html');
-    // do something with it
-    return [];
-};
