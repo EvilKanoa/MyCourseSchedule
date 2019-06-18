2c5b7a7ea89d83e3b880e8b490dcff53b8592311
EvilKanoa <kanoa@kanoa.ca>
2019-06-18 19:18:03 -0400

    Updated react and other deps

--- src/components/layout/App.js
+++ src/components/layout/App.js
@@ -3,8 +3,6 @@ import {withRouter} from 'react-router-dom';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
-import * as Sentry from '@sentry/browser';
-
 import Topbar from 'components/layout/Topbar';
 import Sidebar from 'components/layout/Sidebar';
 import {fetchCourses} from 'reducers/courses';
@@ -21,16 +19,6 @@ import './App.scss';
 class App extends PureComponent {
     componentDidMount() {
         this.props.fetchCourses();
-    }
-
-    componentDidCatch(error, errorInfo) {
-        this.setState({ error });
-        Sentry.withScope(scope => {
-            Object.keys(errorInfo).forEach(key => {
-                scope.setExtra(key, errorInfo[key]);
-            });
-            Sentry.captureException(error);
-        });
     }
 
     render() {
--- src/index.js
+++ src/index.js
@@ -2,24 +2,16 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
 
-import * as Sentry from '@sentry/browser';
-
-import Routes from 'core/routes';
+import routes from 'core/routes';
 import store from 'core/store';
 
 import 'index.scss';
-
-if (process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN) {
-    Sentry.init({
-        dsn: process.env.SENTRY_DSN
-    });
-}
 
 window.getState = store.getState;
 
 render(
     <Provider store={store}>
-        {Routes}
+        {routes}
     </Provider>,
     document.getElementById('root')
 );
